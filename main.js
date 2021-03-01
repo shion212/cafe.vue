05482b4f3b039edf8d5b821c4d233314d5fe253f
イワムラシオン <68310617+shion212@users.noreply.github.com>
2021-03-01 23:38:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,22 @@ $(function(){
         $load = $('#load_animation'),
         $load_in = $load.find('img');
 
-    //ロゴとLoadingGIFをフェードイン
+   
     $load_in.delay(500).fadeIn('slow');
 
-    //ウィンドウに対してloadイベントを設定
-    //全ての要素をロードが終わったら，HideLoadingScreen()を実行する
+    
     $window.on('load',function(){
         HideLoadingScreen();
     })
     
-    //５秒後に強制的にHideLoadingScreen()を実行
+    
     setTimeout(function(){
         HideLoadingScreen();
     },1500);
 
-    //ロード画面を非表示にする関数
+    
     function HideLoadingScreen(){
-        $load.delay(5000).fadeOut("slow");
+        $load.delay(1200).fadeOut("slow");
     }
 }); 
 
